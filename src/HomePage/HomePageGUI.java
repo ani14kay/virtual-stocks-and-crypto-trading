8d57853fb8b4ab968e3b097d8552fb6f23263e9e
Himanshu Kumar Verma <76449638+Himanshuiiit@users.noreply.github.com>
2021-10-07 01:36:19 +0530

    title is added

--- src/HomePage/HomePageGUI.java
+++ src/HomePage/HomePageGUI.java
@@ -10,7 +10,17 @@ public class HomePageGUI extends JFrame {
         this.setSize(1920,1080);// setting this.setVisible(true);// frame visibility
         ImageIcon logo = new ImageIcon("Assets/Icons/logo.png");//creating an ImageIcon
         this.setIconImage(logo.getImage());//icon set
-        this.getContentPane().setBackground(new Color(245,245,255));//bg colour setting
-        this.setVisible(true);
+        this.getContentPane().setBackground(new Color(250,250,255));//bg colour setting
+        this.setLayout(null);//null layout is set
+
+        JLabel name = new JLabel("Stock and Cryptocurrency Management System");//name label is created
+        name.setForeground(new Color(100,100,255));//name label color
+        name.setFont(new Font("Verdana",Font.BOLD,30));//name label font and style
+        name.setVerticalAlignment(JLabel.TOP);//set vertical alignment to top
+        name.setBounds(40,30,1200,100);//layout bonds for name is set
+
+        this.add(name);//logo label added to the HomePageGUI
+        this.setVisible(true);// HomePageGUI visibility
+
     }
 }
