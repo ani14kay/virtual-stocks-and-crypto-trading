18b13aaeec609d82aff944c2f6e5c204c52e3822
Ayushi Shukla <91957156+ayushi-8102@users.noreply.github.com>
2021-12-29 01:41:26 +0530

    Update PortfolioGUI.java

--- src/GUI/PortfolioGUI.java
+++ src/GUI/PortfolioGUI.java
@@ -1,15 +1,25 @@
 package GUI;
 
+
+//import Data.Portfolio;
+//import Data.Stock;
+
+import Data.stock_detail;
+import org.json.JSONException;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
-public class PortfolioGUI extends JPanel {
-    JButton wishlist,market,Home,deposit,withdraw,back,next,s1,s2,s3,s4;
+public class PortfolioGUI extends JPanel implements ActionListener {
+    JButton wishlist,market;
     JButton i1,i2,i3,i4;
+    JButton port,view,purchase,sale;
     JLabel heading;
     JPanel top,bottom,center,grid;
-
+    private String userId;
     PortfolioGUI() {
         //top heading
         heading = new JLabel("WELCOME TO STOCKS PORTFOLIO!", SwingConstants.CENTER);
@@ -23,13 +33,13 @@ public class PortfolioGUI extends JPanel {
         //BUTTON 1
         wishlist = new JButton();
         wishlist.setBounds( 300, 640, 300, 120);
-        // wishlist.addActionListener((ActionListener) this);
+        wishlist.addActionListener( this);
         wishlist.setText("Wishlist");
         wishlist.setFocusable(false);
         wishlist.setFont(new Font("Comic Sans", Font.BOLD, 25));
 
         wishlist.setForeground(Color.black);
-        wishlist.setBackground(Color.cyan);
+        wishlist.setBackground(Color.lightGray);
         wishlist.setBorder(BorderFactory.createEtchedBorder());
         ImageIcon wish = new ImageIcon("Assets/Icons/wishlist.png");//image icon
         wishlist.setIcon(wish);
@@ -37,13 +47,13 @@ public class PortfolioGUI extends JPanel {
 
         //button 2
         market = new JButton();
-        market.setBounds(450, 640, 300, 120);
-        // market.addActionListener((ActionListener) this);
+        market.setBounds(700, 640, 300, 120);
+        market.addActionListener( this);
         market.setText("Market");
         market.setFocusable(false);
         market.setFont(new Font("Comic Sans", Font.BOLD, 25));
         market.setForeground(Color.black);
-        market.setBackground(Color.cyan);
+        market.setBackground(Color.lightGray);
         market.setBorder(BorderFactory.createEtchedBorder());
         ImageIcon mkt = new ImageIcon("Assets/Icons/Market1.png");
         market.setIcon(mkt);
@@ -51,174 +61,63 @@ public class PortfolioGUI extends JPanel {
 
 
 
-        //button 3
-
-        Home = new JButton();
-        Home.setBounds(630, 640, 300, 120);
-        //    Home.addActionListener((ActionListener) this);
-        Home.setText("Home");
-        Home.setFocusable(false);
-        Home.setFont(new Font("Comic Sans", Font.BOLD, 25));
-        Home.setForeground(Color.black);
-        Home.setBackground(Color.cyan);
-        Home.setBorder(BorderFactory.createEtchedBorder());
-        ImageIcon ho = new ImageIcon("Assets/Icons/Home.png");
-        Home.setIcon(ho);
-        Home.setHorizontalTextPosition(JButton.RIGHT);
-
-
-        // //button 4
-        // deposit = new JButton();
-        // deposit.setBounds(280, 100, 180, 70);
-        // //deposit.addActionListener((ActionListener) this);
-        // deposit.setText("Deposit INR");
-        // deposit.setFocusable(false);
-        // deposit.setFont(new Font("Comic Sans", Font.BOLD, 25));
-        // deposit.setForeground(Color.white);
-        // deposit.setBackground(Color.blue);
-        // deposit.setBorder(BorderFactory.createEtchedBorder());
-
-        // //button 4
-        // withdraw = new JButton();
-        // withdraw.setBounds(800, 100, 180, 70);
-        // // withdraw.addActionListener((ActionListener) this);
-        // withdraw.setText("Withdraw INR");
-        // withdraw.setFocusable(false);
-        // withdraw.setFont(new Font("Comic Sans", Font.BOLD, 25));
-        // withdraw.setForeground(Color.black);
-        // withdraw.setBackground(Color.white);
-        // withdraw.setBorder(BorderFactory.createEtchedBorder());
-
-        //button 5
-        back = new JButton();
-        back.setBounds(10, 0, 50, 50);
-        //  back.addActionListener((ActionListener) this);
-        //  back.setText("Back");
-        back.setFocusable(false);
-        back.setBackground(Color.cyan);
-        back.setBorder(BorderFactory.createEtchedBorder());
-        ImageIcon backi = new ImageIcon("Assets/Icons/back.png");
-        back.setIcon(backi);
-        //s1.setHorizontalTextPosition(JButton.RIGHT);
-
-        //button 6
-        next = new JButton();
-        next.setBounds(80, 0, 50, 50);
-        //  next.addActionListener((ActionListener) this);
-        // next.setText("Next");
-        next.setFocusable(false);
-        next.setBackground(Color.cyan);
-        next.setBorder(BorderFactory.createEtchedBorder());
-        ImageIcon nexti = new ImageIcon("Assets/Icons/next.png");
-        next.setIcon(nexti);
-
-        //button 7
-        s1 = new JButton();
-        s1.setBounds(20, 230, 500, 70);
-        s1.setText("  Nifty 50           -1.04%");
-        s1.setFocusable(false);
-        s1.setFont(new Font("Comic Sans", Font.BOLD, 25));
-        s1.setForeground(Color.black);
-        s1.setBackground(Color.lightGray);
-        s1.setBorder(BorderFactory.createEtchedBorder());
-        ImageIcon nifty = new ImageIcon("Assets/Icons/nifty50.png");
-        s1.setIcon(nifty);
-        s1.setHorizontalTextPosition(JButton.RIGHT);
 
 
 
-        //button 8
-        s2 = new JButton();
-        s2.setBounds(20, 310, 500, 70);
-        s2.setText("  Tata Motors        +0.70%");
-        s2.setFocusable(false);
-        s2.setFont(new Font("Comic Sans", Font.BOLD, 25));
-        s2.setForeground(Color.black);
-        s2.setBackground(Color.lightGray);
-        s2.setBorder(BorderFactory.createEtchedBorder());
-        ImageIcon tata = new ImageIcon("Assets/Icons/tata.png");
-        s2.setIcon(tata);
-        s2.setHorizontalTextPosition(JButton.RIGHT);
+        port = new JButton();
+        port.setBounds(20, 230, 500, 70);
+        port.addActionListener( this);
+        port.setText("View Portfolio");
+        port.setFocusable(false);
+        port.setFont(new Font("Comic Sans", Font.BOLD, 25));
+        port.setForeground(Color.white);
+        port.setBackground(Color.blue);
+        port.setBorder(BorderFactory.createEtchedBorder());
+        port.addActionListener( this);
+        port.setHorizontalTextPosition(JButton.RIGHT);
 
 
-        //button 9
-        s3 = new JButton();
-        s3.setBounds(20, 390, 500, 70);
-        // withdraw.addActionListener((ActionListener) this);
-        s3.setText("          Suzuki                +1.14%");
-        s3.setFocusable(false);
-        s3.setFont(new Font("Comic Sans", Font.BOLD, 25));
-        s3.setForeground(Color.black);
-        s3.setBackground(Color.lightGray);
-        s3.setBorder(BorderFactory.createEtchedBorder());
-        ImageIcon suzuki = new ImageIcon("Assets/Icons/Suzuki1.png");
-        s3.setIcon(suzuki);
-        s3.setHorizontalTextPosition(JButton.RIGHT);
+        purchase = new JButton();
+        purchase.setBounds(20, 330, 500, 70);
+        purchase.addActionListener( this);
+        purchase.setText("Purchase History");
+        purchase.setFocusable(false);
+        purchase.setFont(new Font("Comic Sans", Font.BOLD, 25));
+        purchase.setForeground(Color.white);
+        purchase.setBackground(Color.blue);
+        purchase.setBorder(BorderFactory.createEtchedBorder());
 
-        //button 10
-        s4 = new JButton();
-        s4.setBounds(20, 470, 500, 70);
-        // withdraw.addActionListener((ActionListener) this);
-        s4.setText("      ITC                      +1.24%");
-        s4.setFocusable(false);
-        s4.setFont(new Font("Comic Sans", Font.BOLD, 25));
-
-        s4.setForeground(Color.black);
-        s4.setBackground(Color.lightGray);
-        s4.setBorder(BorderFactory.createEtchedBorder());
-        ImageIcon mrf = new ImageIcon("Assets/Icons/itc.jpg");
-        s4.setIcon(mrf);
-        s4.setHorizontalTextPosition(JButton.RIGHT);
+        purchase.setHorizontalTextPosition(JButton.RIGHT);
 
 
-        //button 11
-        i1= new JButton();
-        i1.setBounds(770, 225, 150, 70);
-        i1.setText("INVEST");
-        i1.setFocusable(false);
-        i1.setFont(new Font("Comic Sans", Font.BOLD, 25));
-        i1.setForeground(Color.white);
-        i1.setBackground(Color.blue);
-        i1.setBorder(BorderFactory.createEtchedBorder());
+        sale = new JButton();
+        sale.setBounds(20, 420, 500, 70);
+        sale.addActionListener( this);
+        sale.setText("Sale History");
+        sale.setFocusable(false);
+        sale.setFont(new Font("Comic Sans", Font.BOLD, 25));
+        sale.setForeground(Color.white);
+        sale.setBackground(Color.blue);
+        sale.setBorder(BorderFactory.createEtchedBorder());
 
-        //button 12
-        i2= new JButton();
-        i2.setBounds(770, 305, 150, 70);
-        i2.setText("INVEST");
-        i2.setFocusable(false);
-        i2.setFont(new Font("Comic Sans", Font.BOLD, 25));
-        i2.setForeground(Color.white);
-        i2.setBackground(Color.blue);
-        i2.setBorder(BorderFactory.createEtchedBorder());
+        sale.setHorizontalTextPosition(JButton.RIGHT);
 
-        //button 13
-        i3= new JButton();
-        i3.setBounds(770, 385, 150, 70);
-        i3.setText("INVEST");
-        i3.setFocusable(false);
-        i3.setFont(new Font("Comic Sans", Font.BOLD, 25));
-        i3.setForeground(Color.white);
-        i3.setBackground(Color.blue);
-        i3.setBorder(BorderFactory.createEtchedBorder());
-
-        //button 14
-        i4= new JButton();
-        i4.setBounds(770, 465, 150, 70);
-        i4.setText("INVEST");
-        i4.setFocusable(false);
-        i4.setFont(new Font("Comic Sans", Font.BOLD, 25));
-        i4.setForeground(Color.white);
-        i4.setBackground(Color.blue);
-        i4.setBorder(BorderFactory.createEtchedBorder());
-
-
+        view= new JButton();
+        view.setBounds(770, 305, 300, 70);
+        view.addActionListener( this);
+        view.setText("View Stock Details");
+        view.setFocusable(false);
+        view.setFont(new Font("Comic Sans", Font.BOLD, 25));
+        view.setForeground(Color.white);
+        view.setBackground(Color.blue);
+        view.setBorder(BorderFactory.createEtchedBorder());
 
         //panels
         top = new JPanel();bottom = new JPanel(); center= new JPanel();
 
         top.setBackground(Color.white);
-        bottom.setBackground(Color.gray);
-        center.setBackground(Color.black);
+        bottom.setBackground(Color.white);
+        center.setBackground(Color.white);
 
         top.setBounds(0,0,1200,70);
         bottom.setPreferredSize(new Dimension(50, 50));
@@ -232,19 +131,19 @@ public class PortfolioGUI extends JPanel {
 
         this.add(wishlist);
         this.add(market);
-        this.add(Home);
-        this.add(back);
-        this.add(next);
+        //this.add(Home);
+        //this.add(back);
+        // this.add(next);
         // this.add(deposit);
         // this.add(withdraw);
-        this.add(s1);
-        this.add(s2);
-        this.add(s3);
-        this.add(s4);this.add(i1);this.add(i2);this.add(i3);this.add(i4);
+        this.add(port);
+        this.add(view);
+        this.add(purchase);
+        this.add(sale);//this.add(i1);this.add(i2);this.add(i3);this.add(i4);
         top.add(heading);
         bottom.add(wishlist);
         bottom.add(market);
-        bottom.add(Home);
+        // bottom.add(Home);
 
 
 
@@ -257,4 +156,37 @@ public class PortfolioGUI extends JPanel {
         this.setBounds(300,70,1200,700);
         this.setVisible(true);
     }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        //Portfolio p=null;
+        try {
+            // p=new Portfolio(userId);
+        } catch (Exception e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        if(e.getSource()==wishlist){
+            wishlist wish=new wishlist();
+        }
+
+        if(e.getSource()==market){
+            System.out.println("Showing the market trends!");
+            market mkt= new market();
+        }
+
+        if(e.getSource()==port){
+            view_port portview=new view_port();
+        }
+        if(e.getSource()==view){
+            stock_detail detailview=new stock_detail();
+        }
+
+        //Stock s=new Stock(stock);
+        // p.displayStockPortfolio(s);
+
+    }
 }
+
+
+
