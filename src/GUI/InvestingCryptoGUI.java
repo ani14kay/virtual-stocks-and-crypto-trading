936a44b9907875a6c822930c7416fb673ce13eb7
Himanshu Kumar Verma <76449638+Himanshuiiit@users.noreply.github.com>
2021-12-29 10:14:25 +0530

    temp commit

--- src/GUI/InvestingCryptoGUI.java
+++ src/GUI/InvestingCryptoGUI.java
@@ -15,110 +15,71 @@ public class InvestingCryptoGUI extends JPanel {
     InvestingCryptoGUI() {
         this.setBackground(new Color(250, 250, 255));
         this.setBounds(300, 150, 1200, 600);
-        this.setVisible(true);
 
 
         // Creating data set for table
-        String[] columns = {"Crypto Name","Value","Statistics","Purchase"};
+        String[] columns = {"Crypto Name", "Value", "Statistics"};
         Object[][] d = {
-                { "Sam", "29" ," Twinkle House","buy"},
-                { "Anna Sam", " 27 ", "Happy Villa","buy" },
-                { "Iza Norah", " 4 ", "Happy house" ,"buy"},
-                { "Sam", "29" ," Twinkle House","buy"}
+                {"Sam", "29", " Twinkle House"},
+                {"Anna Sam", " 27 ", "Happy Villa"},
+                {"Iza Norah", " 4 ", "Happy house"},
+                {"Sam", "29", " Twinkle House"}
 
         };
-
         // JTable initialization
-         jt = new JTable(d,columns){};
-        // set custom renderer to teams colums
-        jt.getColumnModel().getColumn(3).setCellRenderer(new ButtonRenderer());
-
-        //ser custom editor
-        jt.getColumnModel().getColumn(3).setCellEditor(new ButtonEditor(new JTextField()));
-         //set bounds for JTable
-
-        jt.setPreferredScrollableViewportSize(new Dimension(1170,570));
-        jt.setFillsViewportHeight(true);
-// add it to the JScrollPane
-        JScrollPane jsp = new JScrollPane(jt);
-        this.add(jsp);
-
-        // Adding jbutton to columns
+        jt = new JTable(d, columns){
+            public boolean isCellEditable(int row, int column){
+                return false;
+            }
+        };
+        jt.setBounds(0, 0, 1200, 500);
 
 
+        JScrollPane scrollPane = new JScrollPane(jt, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBorder(null);
+        scrollPane.setBounds(0, 0, 1200, 500);
+        scrollPane.setBackground(new Color(250, 250, 255));
 
+        JLabel name = new JLabel("Name");
+        name.setBounds(50,520,200,20);
 
+        JTextArea nametxt = new JTextArea();
+        nametxt.setBounds(50,540,200,20);
+        nametxt.setEditable(false);
+        nametxt.setBorder(BorderFactory.createLineBorder(Color.black));
 
+        JLabel value = new JLabel("Value");
+        value.setBounds(320,520,200,20);
+
+        JTextArea valuetxt = new JTextArea();
+        valuetxt.setBounds(320,540,200,20);
+        valuetxt.setEditable(false);
+        valuetxt.setBorder(BorderFactory.createLineBorder(Color.black));
+
+        JLabel changeInValue = new JLabel("Change in value");
+        changeInValue.setBounds(590,520,200,20);
+
+        JTextArea changeInValuetxt = new JTextArea();
+        changeInValuetxt.setBounds(590,540,200,20);
+        changeInValuetxt.setEditable(false);
+        changeInValuetxt.setBorder(BorderFactory.createLineBorder(Color.black));
+
+        JButton buy = new JButton("Buy");
+        buy.setBounds(860,530,130,30);
+        buy.setBackground(new Color(18, 30, 255));
+        buy.setForeground(new Color(255,255,255));
+        buy.setFocusable(false);
+
+        this.add(buy);
+        this.add(changeInValue);
+        this.add(changeInValuetxt);
+        this.add(valuetxt);
+        this.add(value);
+        this.add(name);
+        this.add(nametxt);
+        this.add(scrollPane);
+        this.setVisible(true);
+        this.setLayout(null);
     }
-
-    class ButtonRenderer extends JButton implements TableCellRenderer
-    {
-        public ButtonRenderer() {
-            //set button properties
-            setOpaque(true);
-        }
-        public Component getTableCellRendererComponent(JTable table, Object obj,
-                                                       boolean isSelected, boolean hasFocus, int row, int column) {
-            setText((obj == null) ? "buy" : obj.toString());
-            return this;
-        }
-    }
-    class ButtonEditor extends DefaultCellEditor
-    {
-        private String label;
-        protected JButton btn;
-        private boolean clicked;
-
-        public ButtonEditor(JTextField txt)
-        {
-
-            super(txt);
-            btn = new JButton();
-            btn.setOpaque(true);
-
-            //when button is clicked
-            btn.addActionListener(new ActionListener(){
-
-
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    fireEditingCanceled();
-                }
-            });
-
-
-
-
-        }
-
-
-        public Component getTableCellEditorComponent(JTable table, Object value,
-                                                     boolean isSelected, int row, int column)
-        {
-            label = (value == null) ? "buy" : value.toString();
-            btn.setText(label);
-            clicked = true;
-            return btn;
-        }
-        public Object getCellEditorValue()
-        {
-           if(clicked){
-               //show some message
-               JOptionPane.showMessageDialog(btn,label+"Clicked");
-           }
-           clicked = false;
-           return new String(label);
-        }
-
-        public boolean stopCellEditing(){
-            clicked=false;
-            return super.stopCellEditing();
-        }
-
-        protected void fireEditingStopped(){
-            super.fireEditingStopped();
-        }
-    }
-
-    }
+}
 
