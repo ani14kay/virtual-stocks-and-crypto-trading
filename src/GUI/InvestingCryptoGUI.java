86892aab6e4ec8d68356973deef93b3428e78a46
Himanshu Kumar Verma <76449638+Himanshuiiit@users.noreply.github.com>
2021-12-29 21:42:15 +0530

    temp commit

--- src/GUI/InvestingCryptoGUI.java
+++ src/GUI/InvestingCryptoGUI.java
@@ -10,7 +10,8 @@ import javax.swing.table.*;
 
 public class InvestingCryptoGUI extends JPanel {
     JTable jt;
-    JButton Button;
+    JButton buy;
+    JTextArea nametxt,valuetxt,changeInValuetxt;
 
     InvestingCryptoGUI() {
         this.setBackground(new Color(250, 250, 255));
@@ -26,24 +27,11 @@ public class InvestingCryptoGUI extends JPanel {
                 {"Sam", "29", " Twinkle House"}
 
         };
-        // JTable initialization
-        jt = new JTable(d, columns){
-            public boolean isCellEditable(int row, int column){
-                return false;
-            }
-        };
-        jt.setBounds(0, 0, 1200, 500);
-
-
-        JScrollPane scrollPane = new JScrollPane(jt, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setBorder(null);
-        scrollPane.setBounds(0, 0, 1200, 500);
-        scrollPane.setBackground(new Color(250, 250, 255));
 
         JLabel name = new JLabel("Name");
         name.setBounds(50,520,200,20);
 
-        JTextArea nametxt = new JTextArea();
+        nametxt = new JTextArea();
         nametxt.setBounds(50,540,200,20);
         nametxt.setEditable(false);
         nametxt.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -51,7 +39,7 @@ public class InvestingCryptoGUI extends JPanel {
         JLabel value = new JLabel("Value");
         value.setBounds(320,520,200,20);
 
-        JTextArea valuetxt = new JTextArea();
+        valuetxt = new JTextArea();
         valuetxt.setBounds(320,540,200,20);
         valuetxt.setEditable(false);
         valuetxt.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -59,16 +47,41 @@ public class InvestingCryptoGUI extends JPanel {
         JLabel changeInValue = new JLabel("Change in value");
         changeInValue.setBounds(590,520,200,20);
 
-        JTextArea changeInValuetxt = new JTextArea();
+        changeInValuetxt = new JTextArea();
         changeInValuetxt.setBounds(590,540,200,20);
         changeInValuetxt.setEditable(false);
         changeInValuetxt.setBorder(BorderFactory.createLineBorder(Color.black));
 
-        JButton buy = new JButton("Buy");
+        buy = new JButton("Buy");
         buy.setBounds(860,530,130,30);
         buy.setBackground(new Color(18, 30, 255));
         buy.setForeground(new Color(255,255,255));
         buy.setFocusable(false);
+
+
+        // JTable initialization
+        jt = new JTable(d, columns){
+            public boolean isCellEditable(int row, int column){
+                return false;
+            }
+        };
+        jt.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int i = jt.getSelectedRow();
+                TableModel model = jt.getModel();
+                nametxt.setText(model.getValueAt(i,0).toString());
+                valuetxt.setText(model.getValueAt(i,1).toString());
+                changeInValuetxt.setText(model.getValueAt(i,2).toString());
+            }
+        });
+        jt.setBounds(0, 0, 1200, 500);
+
+
+        JScrollPane scrollPane = new JScrollPane(jt, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBorder(null);
+        scrollPane.setBounds(0, 0, 1200, 500);
+        scrollPane.setBackground(new Color(250, 250, 255));
 
         this.add(buy);
         this.add(changeInValue);
